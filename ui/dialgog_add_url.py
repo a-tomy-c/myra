a3516be988017adb201b6d68224bf09f37ac331d
tomy <213143875+a-tomy-c@users.noreply.github.com>
2025-10-25 22:36:44 -0400

    agregue la ventana modal para agregar items y redimension a los iconos

--- ui/dialgog_add_url.py
+++ ui/dialgog_add_url.py
@@ -1,5 +1,43 @@
-from PySide6.QtWidgets import QDialog, QWidget
+from PySide6.QtWidgets import QDialog, QWidget, QFileDialog, QLabel
+from PySide6.QtGui import QPixmap
+from PySide6.QtCore import Qt, QSize
 from ui.skin_dialog_add_url import Ui_Dialog
+from pathlib import Path
+
+
+class Viewer(QLabel):
+    def __init__(self, *args, **kw):
+        super().__init__(*args, **kw)
+        self._cnf_Viewer()
+
+    def _cnf_Viewer(self):
+        self.IMAGE = None
+        self.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+    def get_image(self) -> str:
+        """retorna la ruta de la image"""
+        return self.IMAGE
+
+    def set_image(self, image:str):
+        """asigna la ruta de la imagen"""
+        self.IMAGE = Path(image).as_posix()
+
+    def get_pixmap(self, size:int=150) -> QPixmap:
+        """retorna QPixmap redimensionado"""
+        if self.get_image():
+            pixmap = QPixmap(self.get_image())
+            if not pixmap.isNull():
+                return pixmap.scaled(
+                    QSize(size, size),
+                    Qt.AspectRatioMode.KeepAspectRatio,
+                    Qt.TransformationMode.SmoothTransformation
+                )
+
+    def resize_image(self, size:int):
+        """asigna un pixmap de tamaño (size) al viewer"""
+        pixmap = self.get_pixmap(size)
+        if pixmap:
+            self.setPixmap(pixmap)
 
 
 class DialogAddUrl(QDialog, Ui_Dialog):
@@ -10,3 +48,41 @@ class DialogAddUrl(QDialog, Ui_Dialog):
 
     def _cnf_DialogAddUrl(self):
         self.setWindowTitle('Agrega una nueva Url')
+        self.viewer = Viewer()
+        self.btn_load_image.clicked.connect(self.select_image)
+
+    def select_image(self):
+        """dialogo para seleccionar una imagen"""
+        self.image_path, _ = QFileDialog.getOpenFileName(
+            self, "Seleciona una Imagen", "",
+            "Imagenes (*.jpg *.png *.jpeg)"
+        )
+        if self.image_path:
+            self.viewer.set_image(image=self.image_path)
+            pix_scaled:QPixmap = self.viewer.get_pixmap()
+            if not pix_scaled.isNull():
+                self.lb_viewer.setPixmap(pix_scaled)
+            else:
+                self.lb_info.setText("ERROR al cargar la imagen")
+
+    def get_image(self) -> str:
+        """retorna la ruta de la imagen"""
+        return self.viewer.get_image()
+    
+    def get_url(self) -> str:
+        """retorna la url ingresada en la ventana modal"""
+        return self.le_url.text()
+    
+    def get_name(self) -> str:
+        """retorna el nombre ingresado en la ventana modal"""
+        return self.le_name.text()
+    
+    def get_data(self) -> tuple[str]:
+        """retorna una tupla: (nombre, url, imagen)"""
+        return self.get_name(), self.get_url(), self.get_image()
+    
+    def get_viewer(self, size:int) -> Viewer:
+        """retorna un objeto Viewer (QLabel con imagen)"""
+        self.viewer.resize_image(size=size)
+        return self.viewer
+
--- ui/widget_playlist.py
+++ ui/widget_playlist.py
@@ -1,9 +1,12 @@
+from pathlib import Path
 from PySide6.QtWidgets import (
     QWidget, QTableWidget, QTableView, QTableWidgetItem, QHeaderView, QAbstractItemView,
-    QDialog, QFileDialog
+    QDialog, QFileDialog, QVBoxLayout, QLabel
 )
+from PySide6.QtGui import QPixmap
+from PySide6.QtCore import QSize, Qt
 from ui.skin_myra_playlist import Ui_Form
-from ui.dialgog_add_url import DialogAddUrl
+from ui.dialgog_add_url import DialogAddUrl, Viewer
 
 
 class WidgetPlaylist(QWidget, Ui_Form):
@@ -18,13 +21,40 @@ class WidgetPlaylist(QWidget, Ui_Form):
        hh.setSectionResizeMode(1, QHeaderView.ResizeMode.ResizeToContents)
        self.sld_size_icons.setRange(24, 60)
 
-
        # agregando dialogo
        self.btn_add.clicked.connect(self.open_dialog_new_url)
-
+       self.sld_size_icons.sliderReleased.connect(self._set_row_height)
 
     def open_dialog_new_url(self):
         dialog = DialogAddUrl(self)
         if dialog.exec() == QDialog.DialogCode.Accepted:
             print("agregada nueva url")
+            name, url, image = dialog.get_data()
+            if url:
+                self.add_item(name, url, image)
+
+    def add_item(self, name:str, url:str, image:str):
+        index_row = self.tw_playlist.rowCount()
+        self.tw_playlist.insertRow(index_row)
+        item_name = QTableWidgetItem(name)
+        item_url = QTableWidgetItem(url)
+        self.tw_playlist.setItem(index_row, 1, item_name)
+        self.tw_playlist.setItem(index_row, 2, item_url)
+        value = self.sld_size_icons.value()
+
+        viewer = Viewer()
+        viewer.set_image(image)
+        viewer.resize_image(size=value)
+        self.tw_playlist.setRowHeight(index_row, value)
+        self.tw_playlist.setCellWidget(index_row, 0, viewer)
+        data = dict(name=name, url=url, image=image)
+        item_name.setData(Qt.ItemDataRole.UserRole, data)
+
+    def _set_row_height(self):
+        value = self.sld_size_icons.value()
+        for index in range(self.tw_playlist.rowCount()):
+            self.tw_playlist.setRowHeight(index, value)
+
+            viewer:Viewer = self.tw_playlist.cellWidget(index, 0)
+            viewer.resize_image(size=value)
 
